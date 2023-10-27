f0fd93d3b71442a989dad8935a9699bae5daa378
phelix <phelix@unavailable.network>
2023-10-27 15:20:39 +0200

    commit messages are for the weak!

--- pc.py
+++ pc.py
@@ -5,6 +5,8 @@ import os
 
 from pprint import pprint, pformat
 
+import argparse
+
 class NoSuchPartException(Exception):
     pass
 
@@ -54,14 +56,44 @@ class Build():
 
 def main():
 
-    parts = yaml.load(open("parts.yml").read(), Loader=yaml.Loader)
-    builds = yaml.load(open("builds.yml").read(), Loader=yaml.Loader)
+    parser = argparse.ArgumentParser(
+        prog='PC Builder',
+        description='build different pc combinations from a set of components',
+    )
+    parser.add_argument('-B', '--buildsfile', metavar="buildsfile", default="builds.yml", help="file containing builds");
+    parser.add_argument('-C', '--componentsfile', metavar="componentsfile", default="components.yml", help="file containing parts");
+    parser.add_argument('-b', '--build', metavar="build", help="use this specific buld");
+    parser.add_argument('-l', '--links', help="open link for build", action="store_true");
+    parser.add_argument('-w', '--webbrowser', metavar="webbrowser", default="firefox", help="open link for build");
+    parser.add_argument('-a', '--all-builds', help="show all builds", action="store_true");
+
+    args = parser.parse_args()
+    pprint(args)
+
+
+    parts = yaml.load(open(args.componentsfile).read(), Loader=yaml.Loader)
+    builds = yaml.load(open(args.buildsfile).read(), Loader=yaml.Loader)
     #pprint(parts)
     #pprint(builds)
-    for name, build in builds.items():
-        b = Build(name, build, parts)
+    if args.all_builds:
+        for name, build in builds.items():
+            b = Build(name, build, parts)
+            b.assemble()
+            print(f"{b}\n")
+    elif args.build is not None:
+        build_data = builds.get(args.build)
+        if build_data is None:
+            print(f"no such build: {args.build}")
+            exit(-2)
+        b = Build(args.build, build_data, parts)
         b.assemble()
         print(f"{b}\n")
+        if args.links:
+            linklist = ' '.join( f"'{link}'" for name, amount, price, link in b.partlist )
+            os.system(f"{args.webbrowser} {linklist}")
+    else:
+        exit(-1)
+
 
 if __name__ == "__main__":
     main()
